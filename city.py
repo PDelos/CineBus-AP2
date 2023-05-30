7ca0f748a0952280c01d4d6382f997733acc96b1
PDelos <132887066+PDelos@users.noreply.github.com>
2023-05-30 21:09:43 +0200

    City PreFinal
    Falta mypy i pip8

--- city.py
+++ city.py
@@ -9,118 +9,143 @@ import pickle
 import matplotlib.pyplot as plt
 import staticmap as stm
 
+
 CityGraph : TypeAlias = nx.DiGraph
 OsmnxGraph : TypeAlias = nx.DiGraph
 BusesGraph : TypeAlias = nx.DiGraph
 
-Coord : TypeAlias = tuple[float, float]   # (latitude, longitude)
+Coord : TypeAlias = tuple[float, float]   # (longitude, latitude)
 Path : TypeAlias = list
 
-def get_osmnx_graph() -> OsmnxGraph:
-	#delete duplicated edges by converting it to DiGraph
-	bcn: OsmnxGraph = nx.DiGraph(ox.graph_from_place("Barcelona", network_type="walk", simplify=True))
 
-	#filter the edge and node information
-	node_attributes = ['x', 'y'] #lat = y, lon = x
-	edge_attributes = ['length'] #u=start, v=end
-	for node_id, node_data in bcn.nodes(data=True):
+def get_osmnx_graph() -> OsmnxGraph:
+	""" Returns procesed Barcelona graph """
+	# Delete duplicated edges by converting it to DiGraph
+	# Same functionality than deleting all edges with key != 0
+	GraphBcn: OsmnxGraph = OsmnxGraph(ox.graph_from_place("Barcelona", network_type="walk", simplify=True))
+
+	# Filter the edge and node information
+	node_attributes = ['x', 'y'] # Information in nodes we whant to keep (x=longitud, y=latitut)
+	edge_attributes = ['length'] # information in edges we whant to keep ()
+
+	# Iterating through nodes
+	for node_id, node_data in GraphBcn.nodes(data=True):
+		# If not in node_attributes we want to delete the atribute
 		for attr in list(node_data):
 			if attr  not in node_attributes:
-				del(bcn.nodes[node_id][attr])
-		bcn.nodes[node_id].update({'color' : '#000000'})
+				del(GraphBcn.nodes[node_id][attr])
 
-	for u, v, edge_data in bcn.edges(data = True):
+		# New information we whant to add to node
+		GraphBcn.nodes[node_id].update({'color' : '#000000'})
+
+	# Iterating through edges
+	for u, v, edge_data in GraphBcn.edges(data = True):
+		# If not in edge_attributes we want to delete the atribute
 		for attr in list(edge_data):
 			if attr  not in edge_attributes:
-				del(bcn[u][v][attr])
-		u_pos: Coord = (bcn.nodes[u]['x'], bcn.nodes[u]['y'])
-		v_pos: Coord = (bcn.nodes[v]['x'], bcn.nodes[v]['y'])
-		bcn[u][v].update({'color' : '#000000', 'route': [u_pos, v_pos]})
+				del(GraphBcn[u][v][attr])
+		GraphBcn[u][v]['length'] /= 1.11 #length is now like time assuming we walk at 4km/h
 
-	return bcn
+		# New information we whant to add to edge
+		u_pos: Coord = (GraphBcn.nodes[u]['x'], GraphBcn.nodes[u]['y'])
+		v_pos: Coord = (GraphBcn.nodes[v]['x'], GraphBcn.nodes[v]['y'])
+		GraphBcn[u][v].update({'color' : '#808080', 'route': [u_pos, v_pos]})
+
+	return GraphBcn
+
 
 def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
-	# guarda el graf g al fitxer filename
+	""" Saves the graph at: filename """
 	with open(filename, "wb") as file:
 		pickle.dump(g, file)
 
 def load_osmnx_graph(filename: str) -> OsmnxGraph:
-	# retorna el graf guardat al fitxer filename
+	""" Loads the graph from: filename """
 	assert os.path.exists(filename)
 	with open(filename, 'rb') as file:
 		return pickle.load(file)
 
 def build_city_graph(bcn: OsmnxGraph, bus: BusesGraph) -> CityGraph:
-	# retorna un graf fusió de g1 i g2
-	#bcn: OsmnxGraph = get_osmnx_graph()
-	#bus: BusesGraph = buses.get_buses_graph()
+	""" Returns the graph of the city (merge of the street graph and the bus graph)"""
+	city_graph: nx.DiGraph = nx.compose(bcn, bus) # We use compose to merge both graphs
 
-	city_graph: nx.DiGraph = nx.compose(bcn, bus)
-
+	# We get the list of nearest nodes from each node of the bus to the bcn graph using ox.distance.nearest_nodes
 	lon_stops: list[float] = [bus.nodes[id]['x'] for id in bus.nodes]
 	lat_stops: list[float] = [bus.nodes[id]['y'] for id in bus.nodes]
 	nearest_cruilla: list[int] = ox.distance.nearest_nodes(bcn, lon_stops, lat_stops, return_dist=False)
+
+	# Create the edge between the stop and its nearest node in bcn graph
 	for stop_id, cruilla_id in zip(bus.nodes, nearest_cruilla):
 		stop_pos: Coord = (bus.nodes[stop_id]['x'], bus.nodes[stop_id]['y'])
 		cruilla_pos: Coord = (bcn.nodes[cruilla_id]['x'], bcn.nodes[cruilla_id]['y'])
-		edge_length = buses.dist(stop_pos, cruilla_pos)
-		#recordem que es directed
+		edge_length = buses.dist(stop_pos, cruilla_pos) #harversine
+		
+		#Since it is directed we want to add both directions
 		city_graph.add_edge(cruilla_id, stop_id, route = [cruilla_pos, stop_pos], length = edge_length, color='#808080')
 		city_graph.add_edge(stop_id, cruilla_id, route = [stop_pos, cruilla_pos], length = edge_length, color='#808080')
+	
 	return city_graph
 	
 
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
-	src_nearest_node = ox.distance.nearest_nodes(ox_g,src[1],src[0])
-	dst_nearest_node = ox.distance.nearest_nodes(ox_g,dst[1],dst[0])
+	""" Given a graph, and 2 points descrived by coordinates, returns the shortest using the graph. Coords: lon, lat """
+	src_nearest_node = ox.distance.nearest_nodes(ox_g,src[0],src[1]) #find the nearest node from src
+	dst_nearest_node = ox.distance.nearest_nodes(ox_g,dst[0],dst[1]) #find the nearest node from dst
 	return nx.shortest_path(g,src_nearest_node,dst_nearest_node, weight='length')
 
 def show(g: CityGraph) -> None:
-	# mostra g de forma interactiva en una finestra
-	fig, ax = plt.subplots()
+	""" Shows the directed graph using networkx.draw """
+	# We extrat the position of each node and create a dictionary
 	pos: dict[Any, float] = {node: (data['x'], data['y']) for node, data in g.nodes(data=True)}
-	nx.draw(g, pos, ax=ax, with_labels=False, node_color='lightblue', edge_color='gray', node_size = 1)
+	# Using the dictionary representing position we plot the graph
+	nx.draw(g, pos, with_labels=False, node_color='lightblue', edge_color='gray', node_size = 1)
 	plt.show()
 
 def plot(g: CityGraph, filename: str) -> None:
+	""" Saves and shows the graph as an image with the background city map in the specified file: filename, using staticmaps library """
 	# Create a new map object
 	barcelona = stm.StaticMap(800, 600)
 	barcelona.center = (2.1734, 41.3851) #staticmap uses 
 
+	# Draws all the nodes in the graph using 'x', 'y' information in edge
 	for id, data in g.nodes(data=True):
 		pos: Coord = (data['x'], data['y'])
 		marker = stm.CircleMarker(pos, data['color'], 2)
 		barcelona.add_marker(marker)
     
+	# Draws all the edges using route information in edge
 	for u, v, data in g.edges(data=True):
 		line = stm.Line(data['route'], data['color'], 1)
 		barcelona.add_line(line)
     
+	# Save image
 	image = barcelona.render()
 	image.save(filename)
+	# Show image
 	image = Image.open(filename)
 	image.show()
 
 def plot_path(g: CityGraph, p: Path, filename: str) -> None:
-	# mostra el camí p en l'arxiu filename
+	""" Saves and shows a path of nodes as an image with the background city map in the specified file: filename, using staticmaps library """
+    # Create a new map object
 	barcelona = stm.StaticMap(800, 600)
 	barcelona.center = (2.1873, 41.3838)
 
-	# Add a marker at a specific location in Barcelona
+	# Add the nodes form the path with the indecated attributes
 	for id in p:
 		pos: Coord = (g.nodes[id]['x'], g.nodes[id]['y'])
-		marker = stm.CircleMarker(pos, g.nodes[id]['color'], 3)
+		marker = stm.CircleMarker(pos, g.nodes[id]['color'], 2)
 		barcelona.add_marker(marker)
 
+	# Add the lines connecting the nodes using the information in the edge u->v
 	for u,v in zip(p,p[1:]):
-		line = stm.Line(g[u][v]['route'], g[u][v]['color'], 2)
+		line = stm.Line(g[u][v]['route'], g[u][v]['color'], 4)
 		barcelona.add_line(line)
 	
+	# Save image
 	image = barcelona.render()
 	image.save(filename)
+	# Show image
+	image = Image.open(filename)
+	image.show()
 
-ox_g=load_osmnx_graph('bcn_graph.pickle')
-city = build_city_graph(ox_g, buses.get_buses_graph())
-
-p: Path = find_path(ox_g,city,(41.401727281643055, 2.151696185862085),(41.38962588196832, 2.167422817271395))
-plot_path(city, p, "short_path.png")
